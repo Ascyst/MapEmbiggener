6b360b37eb7ac8914fb9254b6386689ee386d6df
Ascyst <85650794+Ascyst@users.noreply.github.com>
2021-07-15 20:39:27 -0700

    Add project files.

---
+++ CameraZoomHandler_Patch.cs
@@ -0,0 +1,39 @@
+﻿/*using System;
+using BepInEx;
+using HarmonyLib;
+using Photon.Pun;
+using UnboundLib;
+using UnboundLib.GameModes;
+using UnityEngine;
+using System.Collections;
+using UnboundLib.Networking;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+
+namespace MapEmbiggener
+{
+
+    [HarmonyPatch(typeof(CameraZoomHandler), "Update")]
+    class CameraZoomHandler_Patch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            int insertIndex = -1;
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldfld && codes[i].opcode != )
+                {
+                    insertIndex = i;
+                    codes.Insert(insertIndex, new CodeInstruction(OpCodes.Ldarg_0));
+                    codes.Insert(insertIndex + 1, new CodeInstruction(OpCodes.Ldfld, MapEmbiggener.Mod.setSize));
+                    codes.Insert(insertIndex + 2, new CodeInstruction(OpCodes.Mul));
+                    break;
+                }
+            }
+            return codes;
+        }
+    }
+}*/
---
+++ MapEmbiggener.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+using Photon.Pun;
+using UnboundLib;
+using UnboundLib.GameModes;
+using UnityEngine;
+using System.Collections;
+using UnboundLib.Networking;
+
+namespace MapEmbiggener
+{
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin(ModId, ModName, "1.0.0")]
+    [BepInProcess("Rounds.exe")]
+    public class Mod : BaseUnityPlugin
+    {
+        private struct NetworkEventType
+        {
+            public const string SyncSize = ModId + "_Sync";
+        }
+        private const string ModId = "com.ascyst.rounds.mapembiggener";
+
+        private const string ModName = "Map Embiggener";
+
+        public static float setSize = 1.0f;
+
+        private void Awake()
+        {
+            new Harmony(ModId).PatchAll();
+            NetworkingManager.RegisterEvent(NetworkEventType.SyncSize, sync => setSize = (float)sync[0]);
+        }
+
+        private void Start()
+        {
+            Unbound.RegisterGUI(ModName, DrawGUI);
+            Unbound.RegisterHandshake(ModId, OnHandShakeCompleted);
+        }
+
+        private void DrawGUI()
+        {
+            GUILayout.Label("Current Map Size: x" + setSize);
+            GUILayout.FlexibleSpace();
+            GUILayout.Label("Set Map Size To:");
+            GUILayout.FlexibleSpace();
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("x1.5"))
+            {
+                setSize = 1.5f;
+                Unbound.BuildInfoPopup("Button has been clicked");
+            }
+            if (GUILayout.Button("x2.0"))
+            {
+                setSize = 2.0f;
+            }
+            if (GUILayout.Button("Default"))
+            {
+                setSize = 1.0f;
+            }
+            GUILayout.EndHorizontal();
+
+        }
+
+        private void OnHandShakeCompleted()
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                    NetworkingManager.RaiseEventOthers(NetworkEventType.SyncSize, setSize);
+            }
+        }
+
+    }
+}
