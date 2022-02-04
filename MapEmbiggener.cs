3f4064485d19f2784117356a1e685538d93da8a2
pdcook <qphysicscook@gmail.com>
2022-02-03 21:01:19 -0500

    v2.0.0

--- MapEmbiggener.cs
+++ MapEmbiggener.cs
@@ -19,7 +19,7 @@ using MapEmbiggener.Controllers;
 namespace MapEmbiggener
 {
     [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
-    [BepInPlugin(MapEmbiggener.ModId, MapEmbiggener.ModName, "1.2.9")]
+    [BepInPlugin(MapEmbiggener.ModId, MapEmbiggener.ModName, "2.0.0")]
     [BepInProcess("Rounds.exe")]
     public class MapEmbiggener : BaseUnityPlugin
     {
